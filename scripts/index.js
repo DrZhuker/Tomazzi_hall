bd9f6a3f87fdb28fe4f0e0e4d7fc57db7063abba
DrZhuker <151435401+DrZhuker@users.noreply.github.com>
2024-08-31 22:55:27 +0000

    Deploying to gh-pages from @ DrZhuker/Tomazzi_hall@a6e41a4689f2e55fcd672f239edc6f8aabe312ee 🚀

---
+++ scripts/index.js
@@ -0,0 +1,1 @@
+var t=document.querySelector(".navigation__drop"),o=document.querySelector(".navigation__drop-list");t&&o&&t.addEventListener("click",e=>{e.preventDefault(),o.classList.toggle("navigation__drop-list--show")});
